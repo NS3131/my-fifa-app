58fcd3b6c5a7f485254e7348c3d79efb3d0e71af
NS3131 <shaumba@hotmail.com>
2025-09-07 20:37:01 +0100

    add players name

---
+++ src/data/players.js
@@ -0,0 +1,44 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentina",
+    jerseyNumber: 10,
+    age: 36,
+    imageUrl: "https://tse4.mm.bing.net/th/id/OIP.kr8RamFz03qIW6w5KNZqTwHaFN?w=474&h=474&c=7&p=0"
+  },
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    nationality: "Norway",
+    jerseyNumber: 9,
+    age: 25,
+    imageUrl: "https://i.imgur.chttps://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcRzxvLb1i5_VuJTmoN10_b4fD8F6WLkmtucWDXOc253dl0mXUEQBKh2KysEXwsPl7Uom9sIbvM2I2Uj6Dsom/7mW4tAZ.jpg"
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "PSG",
+    nationality: "France",
+    jerseyNumber: 7,
+    age: 26,
+    imageUrl: "https://i.pinimg.com/736x/be/4f/31/be4f3127554f41cac90edeac6563f235.jpg"
+  },
+  {
+    name: "Jude Bellingham",
+    team: "Real Madrid",
+    nationality: "England",
+    jerseyNumber: 5,
+    age: 22,
+    imageUrl: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRLFSKHiQJYfmkriQFKAvLw8nXeUv-KwaD3h8132Wcqsyv7OzN75KuYrUB55YFLdZ-Px4Hwa8b4kqWpszbYh_heO8asOtFkLfBkEeJuEp4"
+  },
+  {
+    name: "Vinícius Júnior",
+    team: "Real Madrid",
+    nationality: "Brazil",
+    jerseyNumber: 7,
+    age: 24,
+    imageUrl: "https://icdn.football-espana.net/wp-content/uploads/2025/02/Vinicius.jpg"
+  }
+];
+
+export default players;
